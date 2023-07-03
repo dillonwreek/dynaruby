4a2236b3641660372f5f63b71df93f827797eae2
dillonwreek <dillonwreek@gmail.com>
2023-07-03 15:47:03 +0200

    started managing conf file

--- dynaruby.rb
+++ dynaruby.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 def main_page
-  set_args
+  puts "DynaRuby is a client written in Ruby that allows you to update any given NOIP hostname"
+  puts "version 0.1. Made with love by Dillon"
+  conf_file = "/etc/dynaruby.conf"
+  if !File.exist?(conf_file)
+    set_args
+  else
+    config = File.read(conf_file)
+  end
 end
 
 def set_args
@@ -11,7 +18,9 @@ def set_args
 
   puts "please input hostname(s). Tell me you're done with 'c' "
   hostnames = []
+  n = 0
   loop do
+    puts "hostname #{n + 1}: "
     hostnames << gets.chomp
     conf = hostnames.last
     break if hostnames.last == "c"
@@ -19,9 +28,10 @@ def set_args
       puts "wrong escape character. Please input c to confirm."
       hostnames.pop
     end
+    n += 1
   end
   hostnames.pop
-  hostnames_str = hostnames.to_s.gsub("[", "").gsub("]", "").gsub(/"/, "").gsub(",", " ")
+  hostnames_str = hostnames.to_s.gsub("[", "").gsub("]", "").gsub(/"/, "").gsub(",", "")
   check_args(username, password, hostnames_str)
 end
 
@@ -37,6 +47,11 @@ def check_args(username, password, hostnames_str)
     if answer != "y" && answer != "n"
       puts "please input y or n to confirm the args."
     end
+
+    `touch /etc/dynaruby.conf`
+    config = File.open("/etc/dynaruby.conf", "w")
+    config.write("#{username}\n#{password}\n#{hostnames_str}")
+    config.close
   end
 end
 
