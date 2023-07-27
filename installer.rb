bad663689e4440c7c805b21cd13cea67f3134a51
dillonwreek <dillonwreek@gmail.com>
2023-07-27 13:05:04 +0200

    redoing stuff

--- installer.rb
+++ installer.rb
@@ -20,6 +20,7 @@ module Installer
   end
 
   def yes_or_no
+    catch_ctrl_c
     positive_answers = ["y", "yes"]
     negative_answers = ["n", "no"]
     result = nil
@@ -30,12 +31,41 @@ module Installer
     result
   end
 
-  def os
-    case RUBY_PLATFORM
-    when /linux/
-      "linux"
-    when /bsd/
-      "bsd"
+  def set_args
+    catch_ctrl_c
+    #config structure is:
+    #refresh time=
+    #input
+    #username=
+    #input
+    #password=
+    #input
+    #hostnames=
+    #input
+    #input
+    #..
+    config = []
+    puts "Welcome, let's get started"
+    puts "Please input how many times you want to check for an ip change:"
+    config[0] = "refresh time="
+    config[1] = STDIN.gets.chomp.to_i
+    puts "Please input username:"
+    config[2] = "username="
+    config[3] = STDIN.gets.chomp
+    puts "Please input password:"
+    config[4] = "password="
+    config[5] = STDIN.noecho(&:gets).chomp
+    config[6] = "hostnames="
+    puts "Please input hostname(s), tell me you're done with an empty line. Submit d to delete the last inputted hostname"
+    until config.last == ""
+      config << STDIN.gets.chomp
+      config.last == "d" && config.size > 5 ? (puts "hostname removed."; config.pop; config.pop) : nil
     end
+    config.pop
+    config_file = File.open("/etc/dynaruby.conf", "w")
+    config.each do |line|
+      config_file.write("#{line}\n")
+    end
+    config_file.close
   end
 end
